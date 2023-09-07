897105f5578e437dff4423312ed421b02ab18e0b
zero <teclaizerai@gmail.com>
2023-09-07 11:40:41 +0200

    init docs (#13)
    * Conf. and fix php doc header.
    
    * Added couscous as tool and init docs.

---
+++ docs/developer-guide/quick-start.md
@@ -0,0 +1,24 @@
+# Quick start
+
+1. For this project you need to have installed on your machine:
+
+    - Bash (same here, just try `bash --version` in a terminal).
+    - Git (run `git --version` to see if you already have it installed).
+    - A PHP IDE, preferably PhpStorm.
+
+
+2. Clone this project to your machine:
+
+    ```bash
+    git clone https://github.com/MedicalMundi/oe-module-npi-registry.git
+    ```
+
+3. Next, `cd` into the project directory and run:
+
+    ```bash
+    composer install
+    ```
+
+Now you are ready to work.
+
+
--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the medicalmundi/oe-module-npi-registry
+ *
+ * @copyright (c) Zerai Teclai <teclaizerai@gmail.com>.
+ * @copyright (c) Francesca Bonadonna <francescabonadonna@gmail.com>.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/oe-module-npi-registry/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/oe-module-npi-registry/blob/main/LICENSE MIT
+ */
+
 require_once __DIR__ . '/../src/Module.php';
 
 use OpenEMR\Modules\NpiRegistry\Adapter\Http\Web\DefaultController;
